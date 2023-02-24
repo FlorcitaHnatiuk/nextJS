671e577a95d82e0e77b423f8e72a510608ea5e7d
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2023-02-24 17:11:42 -0300

    layouts

---
+++ 02-pokemon-static/pokemon-static/components/layouts/Layout.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import Head from 'next/head';
+
+interface Props {
+    children: String;
+    title?: string;
+}
+
+export const Layout: FC<Props> = ({ children, title }) => {
+    return (
+        <>
+            <Head>
+                <title>{title || 'PokemonApp'}</title>
+                <meta name="author" content="Florencia Hnatiuk" />
+                <meta name="description" content={`Información sobre el pokémon ${title}`} />
+                <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+            </Head>
+            <main style={{
+                padding: '20px 20px'
+            }}>
+                {children}
+            </main>
+        </>
+    )
+};
--- 02-pokemon-static/pokemon-static/pages/index.tsx
+++ 02-pokemon-static/pokemon-static/pages/index.tsx
@@ -1,14 +1,13 @@
+import { Layout } from '../components/layouts'
 import { Button } from '@nextui-org/react'
 import { Inter } from 'next/font/google'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello world!</h1>
-      <Button color="gradient">
-        Welcome
-      </Button>
-    </>
+    <Layout title='Listado de Pokemons'>
+      <Button color="gradient">Hello world</Button>
+    </Layout>
   )
 }
