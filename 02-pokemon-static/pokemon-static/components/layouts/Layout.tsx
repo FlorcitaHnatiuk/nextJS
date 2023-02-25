b5a8107975cd59d2956574a7c7b30311e8f64969
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2023-02-25 13:39:29 -0300

    navbar

--- 02-pokemon-static/pokemon-static/components/layouts/Layout.tsx
+++ 02-pokemon-static/pokemon-static/components/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import Head from 'next/head';
+import { Navbar } from '../ui';
 
 interface Props {
     children: String;
@@ -15,6 +16,7 @@ export const Layout: FC<Props> = ({ children, title }) => {
                 <meta name="description" content={`Información sobre el pokémon ${title}`} />
                 <meta name="keywords" content={`${title}, pokemon, pokedex`} />
             </Head>
+            <Navbar/>
             <main style={{
                 padding: '20px 20px'
             }}>
