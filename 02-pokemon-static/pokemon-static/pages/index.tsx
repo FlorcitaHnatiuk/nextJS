113fd8d04ba2403b4b998cae0a526e746f39371c
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2023-02-27 13:21:03 -0300

    axios + pokeApi SS

--- 02-pokemon-static/pokemon-static/pages/index.tsx
+++ 02-pokemon-static/pokemon-static/pages/index.tsx
@@ -1,13 +1,30 @@
 import { Layout } from '../components/layouts'
-import { Button } from '@nextui-org/react'
+import { NextPage, GetStaticProps  } from 'next'
+import { pokeApi } from '@/api'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const Home: NextPage = (props) => {
+  console.log(props)
   return (
     <Layout title='Listado de Pokemons'>
-      <Button color="gradient">Hello world</Button>
+      <ul>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+      </ul>
     </Layout>
   )
 }
+
+// Esto es del lado del servidor, por ende se estará cargando previamente
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const {data} = await pokeApi.get('/pokemon?limit=151')
+  //console.log(data)
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
+}
+export default Home;
